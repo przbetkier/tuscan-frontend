84e7aaa614c8e16f0571b628bcf597de4e5edcfa
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2018-09-17 12:55:50 +0200

    Tuscan production urls temporary hardcoded for test deployment

--- src/app/services/tuscan.service.ts
+++ src/app/services/tuscan.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 import {Player} from '../model/player.model';
 import {Matches} from '../model/matches.model';
 import {HttpClient} from '@angular/common/http';
@@ -12,22 +12,22 @@ import {PlayerStats} from '../model/player-stats.model';
 export class TuscanService {
 
   constructor(private http: HttpClient) {
-   }
-
-   public getPlayerDetails(nickname: string): Observable<Player> {
-     return this.http.get<Player>('http://localhost:8080/faceit/players/details?nickname=' + nickname);
-   }
-
-  public getPlayerOverallStats(playerId: string): Observable<PlayerStats> {
-    return this.http.get<PlayerStats>(`http://localhost:8080/faceit/players/details/csgo/${playerId}`);
   }
 
-   public getPlayerMatches(playerId: string, offset: number): Observable<Matches> {
-    return this.http.get<Matches>(`http://localhost:8080/faceit/matches/simple?playerId=${playerId}&offset=${offset}`);
+  public getPlayerDetails(nickname: string): Observable<Player> {
+    return this.http.get<Player>('https://tuscan-service.herokuapp.com/faceit/players/details?nickname=' + nickname);
+  }
+
+  public getPlayerOverallStats(playerId: string): Observable<PlayerStats> {
+    return this.http.get<PlayerStats>(`https://tuscan-service.herokuapp.com/faceit/players/details/csgo/${playerId}`);
+  }
+
+  public getPlayerMatches(playerId: string, offset: number): Observable<Matches> {
+    return this.http.get<Matches>(`https://tuscan-service.herokuapp.com/faceit/matches/simple?playerId=${playerId}&offset=${offset}`);
   }
 
   public getMatchDetails(playerId: string, matchId: string): Observable<MatchDetails> {
-    return this.http.get<MatchDetails>(`http://localhost:8080/faceit/matches?playerId=${playerId}&matchId=${matchId}`);
+    return this.http.get<MatchDetails>(`https://tuscan-service.herokuapp.com/faceit/matches?playerId=${playerId}&matchId=${matchId}`);
   }
 
 }
