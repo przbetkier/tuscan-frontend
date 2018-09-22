7d421ab0744072db3c950e8b197da335f27bf3bb
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2018-09-22 18:57:07 +0200

    Forcing ssl - second attempt

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const forceSsl = require('force-ssl-heroku');
 const app = express();
 const path = require('path');
 
+app.use(forceSsl);
+
 app.listen(process.env.PORT || 8080);
 
-app.use((function (req, res) {
-  if (req.get('X-Forwarded-Proto') === 'https' || req.hostname === 'localhost') {
-    express.static(__dirname + '/dist/tuscan-frontend')
-  } else if (req.get('X-Forwarded-Proto') !== 'https' && req.get('X-Forwarded-Port') !== '443') {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-
-}));
+app.use(express.static(__dirname + '/dist/tuscan-frontend'));
 
 // PathLocationStrategy
 
