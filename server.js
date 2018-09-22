6df60cb34c3876be54f55c809d00b9ead424df53
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2018-09-22 18:40:17 +0200

    Resolved too many redirects error

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require('path');
 
 app.listen(process.env.PORT || 8080);
 
-app.use((function (req, res, next) {
-  if (req.secure) {
+app.use((function (req, res) {
+  if (req.get('X-Forwarded-Proto') === 'https' || req.hostname === 'localhost') {
     express.static(__dirname + '/dist/tuscan-frontend')
-  } else {
+  } else if (req.get('X-Forwarded-Proto') !== 'https' && req.get('X-Forwarded-Port') !== '443') {
     res.redirect('https://' + req.headers.host + req.url);
-
   }
+
 }));
 
 // PathLocationStrategy
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/tuscan-frontend/index.html'));
 });
