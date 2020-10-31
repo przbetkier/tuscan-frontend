40c64c365ca7891e56cfa5be7cb08f1a69f9119e
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2020-10-31 04:09:24 +0100

    Adjustments. Typescript moved to dev deps

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Here you can find source code of its frontend layer.
 
 - Player details: presents basic information about the player - elo, k/d, country, etc
 - Map performance: displays how player performs on particular maps in active map pool and points out weaknesses and strengths
-- Recent performance: this section will tell you how subjected player performed during last 20 matches. Here you can find graphs with elo, k/d and HS progress throughout these games
-- Recent matches: will allow you quickly (and without any page reloads) look through recent matches and check performance of any player in the match. On the right you can see number of points gained/lost after this game
+- Recent performance: this section will tell you how subjected player performed during last 20 matches. Here you can find graphs with elo, k/d and HS progress throughout these games.
+- Recent matches: will allow you quickly (and without any page reloads) look through recent matches and check performance of any player in the match. On the right you can see number of points gained/lost after this game.
 
 ![Screenshot](src/assets/presentation/screenshot.png)
 
@@ -29,4 +29,4 @@ All contributions are welcome.
 ### Related projects:
 
 - [tuscan](https://github.com/przbetkier/tuscan) - Java backend service providing player details using FACEIT API.
-- [faceit-lobby-extension-chrome](https://github.com/przbetkier/faceit-lobby-extension-chrome)- Google Chrome extension which helps during map picking on Faceit CS:GO matches.
+- [faceit-lobby-extension-chrome](https://github.com/przbetkier/faceit-lobby-extension-chrome) - Google Chrome extension which helps during map picking on Faceit CS:GO matches.
--- src/app/components/common/progress-bar/progress-bar.component.ts
+++ src/app/components/common/progress-bar/progress-bar.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {LevelInterval} from '@models/level-interval.model';
+import { Component, Input, OnInit } from '@angular/core';
+import { LevelInterval } from '@models/level-interval.model';
 
 @Component({
   selector: 'app-progress-bar',
@@ -11,37 +11,34 @@ export class ProgressBarComponent implements OnInit {
   @Input() elo: number;
   @Input() level: number;
 
-  constructor() {
-  }
+  intervals: LevelInterval[] = [
+    new LevelInterval(1, 0, 800),
+    new LevelInterval(2, 801, 951),
+    new LevelInterval(3, 951, 1101),
+    new LevelInterval(4, 1101, 1251),
+    new LevelInterval(5, 1251, 1401),
+    new LevelInterval(6, 1401, 1551),
+    new LevelInterval(7, 1551, 1701),
+    new LevelInterval(8, 1701, 1851),
+    new LevelInterval(9, 1851, 2000),
+    new LevelInterval(10, 2000, 3000)
+  ];
 
   ngOnInit() {
     const elem = document.getElementById('myBar');
 
     let totalProgress;
-    let intervals: LevelInterval[] = [];
-    intervals = intervals.concat(
-      new LevelInterval(1, 0, 800),
-      new LevelInterval(2, 801, 951),
-      new LevelInterval(3, 951, 1101),
-      new LevelInterval(4, 1101, 1251),
-      new LevelInterval(5, 1251, 1401),
-      new LevelInterval(6, 1401, 1551),
-      new LevelInterval(7, 1551, 1701),
-      new LevelInterval(8, 1701, 1851),
-      new LevelInterval(9, 1851, 2000),
-      new LevelInterval(10, 2000, 3000)
-    );
 
     if (this.level > 1 && this.level < 10) {
-      const difference = this.elo - intervals[this.level - 1].base;
+      const difference = this.elo - this.intervals[this.level - 1].base;
       const progress = (difference / 150) * 10;
       totalProgress = ((this.level - 1) * 10) + progress;
     } else if (this.level === 1) {
-      const difference = this.elo - intervals[0].base;
+      const difference = this.elo - this.intervals[0].base;
       const progress = (difference / 800) * 10;
       totalProgress = ((this.level - 1) * 10) + progress;
     } else if (this.level === 10 && this.elo < 3000) {
-      const difference = this.elo - intervals[this.level - 1].base;
+      const difference = this.elo - this.intervals[this.level - 1].base;
       const progress = (difference / 1000) * 10;
       totalProgress = ((this.level - 1) * 10) + progress;
     } else if (this.level === 10 && this.elo >= 3000) {
@@ -56,9 +53,8 @@ export class ProgressBarComponent implements OnInit {
         clearInterval(id);
       } else {
         width++;
-        elem.style.width = width + '%';
+        elem.style.width = `${width}%`;
       }
     }
   }
-
 }
